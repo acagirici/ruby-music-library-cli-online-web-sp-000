529065bf7ddb465482a18f2a7baf73f9b07bbced
Arif Cagirici <acagirici23@gmail.com>
2020-07-07 04:07:56 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,9 @@
 class MusicLibraryController
-  
+  extend Concerns::Findable
+
+  def initialize(path = "./db/mp3s")
+    new_importer_object = MusicImporter.new(path)
+    new_importer_object.import
+  end
 end
 
